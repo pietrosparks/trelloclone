2fa565fe13225f7b7786c504bad4db0e995c1423
Idakwoji Theophilus <pietrosparks@gmail.com>
2018-04-03 16:35:41 +0100

    de

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,8 @@ export default new Router({
     {
       path: '/',
       name: 'Main',
-      component: Main
+      component: Main,
+      alias:'/trelloclone'
     },
     {
       path: '/:id',
